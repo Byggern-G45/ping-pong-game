dae122b054aad7893cd1781d7edfc7cc5f6113d6
Kristiss99 <keltius87@gmail.com>
2022-09-09 09:49:35 +0200

    Added espens libs

---
+++ Lab_integrerte_kretser/Lab_integrerte_kretser/Libraries/atmega162_usart.c
@@ -0,0 +1,30 @@
+/* atmega162_usart.c -- See atmega162_usart.h for info */
+
+#define atmega162_usart_IMPORT
+#include "atmega162_usart.h"
+
+void atmega162_usart_init() {
+    UBRR0H = (unsigned char)(UBRR>>8);               // Set BAUD rate
+    UBRR0L = (unsigned char)UBRR;
+    UCSR0B = (1<<RXEN0) | (1<<TXEN0);                // Enable receiver and transmitter
+    UCSR0C = (1<<URSEL0) | (1<<USBS0) | (3<<UCSZ00); // Set frame format: 8 data bit, 2 stop bit
+}
+
+void atmega162_usart_transmit(unsigned char character) {
+	while (!(UCSR0A & (1<<UDRE0))); // Wait for empty transmit buffer
+	UDR0 = character;               // Put character into outgoing buffer
+}
+
+unsigned char atmega162_usart_receive() {
+    if (UCSR0A & (1<<RXC0)) { // If character has been received
+        return UDR0;          // Get and return character from buffer
+    }
+    return '\0'               // Notify no character received
+}
+
+void atmega162_usart_flush() {
+    unsigned char dump;
+    while (UCSR0A & (1<<RXC0)) {
+        dump = UDR0;
+    } 
+}
---
+++ Lab_integrerte_kretser/Lab_integrerte_kretser/Libraries/atmega162_usart.h
@@ -0,0 +1,53 @@
+/**
+ * @copyright
+ * @license MIT
+ * @author
+ * @version
+ * @file
+ */
+
+#ifndef atmega162_usart_H
+#define atmega162_usart_H
+
+#include <avr/io.h>
+
+#ifdef atmega162_usart_IMPORT
+    #define EXTERN
+#else
+    #define EXTERN extern
+#endif
+
+#define ATMEGA162_USART_FOSC 4915200 // External oscillator
+#ifndef ATMEGA162_USART_BAUD
+    #define ATMEGA162_USART_BAUD 9600
+#endif
+#define ATMEGA162_USART_PRESCALER 16
+#define UBRR ATMEGA162_USART_FOSC/(ATMEGA162_USART_PRESCALER*ATMEGA162_USART_BAUD) - 1
+
+/**
+ * @brief Initilizes the USART for transmit and receive with 8 data bits and 2 stop bits
+ */
+EXTERN void atmega162_usart_init();
+
+/**
+ * @brief 
+ * 
+ * @param character 
+ */
+EXTERN void atmega162_usart_transmit(unsigned char character);
+
+/**
+ * @brief 
+ * 
+ * @return unsigned char 
+ */
+EXTERN unsigned char atmega162_usart_receive();
+
+/**
+ * @brief Empties the receive buffer
+ */
+EXTERN void atmega162_usart_flush();
+
+#undef atmega162_usart_IMPORT
+#undef EXTERN
+#endif
