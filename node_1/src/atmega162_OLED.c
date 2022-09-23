542ceff074b58940d37f166e72cb1dd1e48c6a20
Birger Maelen <birger.99@live.no>
2022-09-23 12:50:39 +0200

    Added oled file for the functions to use the oled

---
+++ node_1/src/atmega162_OLED.c
@@ -0,0 +1,36 @@
+
+
+void oled_init() {
+
+}
+
+void oled_reset() {
+
+}
+
+void oled_home() {
+
+}
+
+void oled_goto_line(line) {
+
+}
+
+void oled_goto_column(column) {
+
+}
+
+void oled_clear_line(line) {
+
+}
+
+void oled__pos(row_column){
+
+}
+
+void oled_print(char*) {
+    
+}
+
+
+
