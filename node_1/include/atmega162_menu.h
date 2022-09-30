10b01e89550e0e30e4928adb0d935bb7a5675336
espi3000 <espen.holsen.eh@gmail.com>
2022-09-30 14:08:31 +0200

    Made library

---
+++ node_1/include/atmega162_menu.h
@@ -0,0 +1,20 @@
+/**
+ * Library for menu on OLED using ATmega162.
+ */
+
+#ifndef ATMEGA162_MENU_H
+#define ATMEGA162_MENU_H
+
+#include <avr/io.h>
+
+#ifdef ATMEGA162_MENU_IMPORT
+    #define EXTERN
+#else
+    #define EXTERN extern
+#endif
+
+
+
+#undef ATMEGA162_MENU_IMPORT
+#undef EXTERN
+#endif
---
+++ node_1/src/atmega162_menu.c
@@ -0,0 +1,5 @@
+/* atmega162_menu.c -- See atmega162_menu.h for info */
+
+#define ATMEGA162_MENU_IMPORT
+#include "../include/atmega162_menu.h"
+
