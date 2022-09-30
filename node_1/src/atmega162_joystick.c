215983ce035531ce777c24e537bdad3e7a1bc191
Kristiss99 <keltius87@gmail.com>
2022-09-30 08:36:06 +0200

    Prettyed up joystick

--- node_1/src/atmega162_joystick.c
+++ node_1/src/atmega162_joystick.c
@@ -2,11 +2,8 @@
 
 #define ATMEGA162_JOYSTICK_IMPORT
 #include "../include/atmega162_joystick.h"
-//#include "util/delay.h"
-//#define F_CPU 49000000000
 
 #define ADC_START_ADDRESS (volatile char*)0x1400
-//volatile char* ADC_START_ADDRESS = 0x1400;
 #define ATMEGA162_JOYSTICK_CHANNELS 4
 
 struct{
@@ -20,30 +17,25 @@ ISR(INT2_vect) {
 }
 
 ISR(INT1_vect) {
-	for (uint8_t i =0; i<=254;i++)
+	for (uint8_t i = 0; i <= 254;i++)
 	{
 		printf("HELG!\n");
 	}
 	
 }
 ISR(INT0_vect) {
-	for (uint8_t i =0; i<=254;i++)
+	for (uint8_t i = 0; i < =254; i++)
 	{
-		printf("TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!TISS!\n");
+		printf("TISS!\n");
 	}
-	
 }
 
-
-
-// ISR(INT2_vect) {}
 
 void atmega162_joystick_init() {
 	joystick.conversion_done = 1;
 	DDRD |= (1<<DDD4);					// Set Clk as output
 	DDRD = (1 << PD4);
 	TCCR3A|=(1<<WGM30)|(1<<WGM31)|(1<<COM3A0)|(1<<CS30);
-	//TCCR3A |= (1<<COM3A0) | (1<<WGM30);
 	TCCR3B |= (1<<WGM33) | (1<<CS30);
 	
 	cli();								// Disable global interrupt to hinder unwanted interrupt
@@ -71,16 +63,6 @@ void atmega162_joystick_read() {
 		uint8_t byte = (uint8_t)(*ADC_START_ADDRESS);
 		joystick.position[i] = byte;
 		}
+		
 	printf("Posisjon x = %d, y = %d, z = %d, # = %d\n", joystick.position[0]*100/255,joystick.position[1],joystick.position[2],joystick.position[3]);
-	
-		//_delay_us(10);
-		//PORTD = (1<<PD7);
 }
-
-/*
-void adc_to_angle(uint8_t A0, uint8_t A1){
-	
-	int x = 
-	int y =
-}
-*/
