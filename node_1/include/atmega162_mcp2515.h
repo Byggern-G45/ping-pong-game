26477354f420c802952c8919ed32eced3e0cb129
Kristiss99 <keltius87@gmail.com>
2022-10-14 14:43:23 +0200

    Finishing touches, enable/disable debug print

--- node_1/include/atmega162_mcp2515.h
+++ node_1/include/atmega162_mcp2515.h
@@ -6,6 +6,8 @@
 #define ATMEGA162_MCP2515_H
 
 
+
+
 #include "../include/atmega162_spi.h"
 #include <avr/io.h>
 #include <stdio.h>	// Needed for printf
@@ -15,6 +17,44 @@
 #else
     #define EXTERN extern
 #endif
+
+#if 1
+  #define debugging(a) printf a
+#else
+  #define debugging(a) (void)0
+#endif
+
+
+#define RESET 0b11000000 //Resets internal registers to default state, set Configuration mode.
+#define READ 0b00000011 //Read data from register beginning at selected address.
+#define READ_RX_BUFFER 0b10010000//0nm0 //When reading a receive buffer, reduces the overhead of a normal Read command by placing the Address Pointer at one of four locations, as indicated by ‘n,m’. Note: The associated RX flag bit (CANINTF.RXnIF) will be cleared after bringing CS high.
+#define RX_BUFFER0 0b00000000
+#define RX_BUFFER1 0b00000001
+#define RX_BUFFER_FROMTOP 0b00000001
+#define RX_BUFFER_FROMBOTTOM 0b00000000
+#define WRITE 0b00000010 //Write data to register beginning at selected address.
+#define LOAD_TX_BUFFER 0b01000000 //0abc //When loading a transmit buffer, reduces the overhead of a normal Write command by placing the Address Pointer at one of six locations as indicated by ‘a,b,c’.
+#define TX_BUFFER0 0b00000000
+#define TX_BUFFER1 0b00000001
+#define TX_BUFFER2 0b00000010
+#define TX_BUFFER_FROMTOP 0b00000001
+#define TX_BUFFER_FROMBOTTOM 0b00000000
+#define RTS 0b10000000 //0nnn //Instructs controller to begin message transmission sequence for any of the transmit buffers.
+#define RTS_BUFFER0 0b00000001
+#define RTS_BUFFER1 0b00000010
+#define RTS_BUFFER2 0b00000100
+#define READ_STATUS 0b10100000 //Quick polling command that reads several status bits for transmit and receive functions.
+#define RX_STATUS 0b10110000 //Quick polling command that indicates filter match and message type (standard, extended and/or remote) of received message.
+#define BIT_MODIFY 0b00000101 
+#define Normal_Operation_mode 0b000
+#define Sleep_mode 0b001 //= Set Sleep mode
+#define Loopback_mode 0b01000000 //= Set Loopback mode
+#define Listen_mode 0b011 //= Set Listen-Only mode
+#define Configuration_mode 0b100 //= Set Configuration mode
+#define Mode_address 0x0e //upper data for mode register, next selects bits to be modified
+#define Mode_mask 0b11100000 //Mask for mode register
+#define Mode_data 0b00000000 //Data for mode register
+
 
 void mcp_init();
 
--- node_1/src/atmega162_mcp2515.c
+++ node_1/src/atmega162_mcp2515.c
@@ -4,48 +4,11 @@
 #include "../include/atmega162_mcp2515.h"
 
 
-#define RESET 0b11000000 //Resets internal registers to default state, set Configuration mode.
-#define READ 0b00000011 //Read data from register beginning at selected address.
-#define READ_RX_BUFFER 0b10010000//0nm0 //When reading a receive buffer, reduces the overhead of a normal Read command by placing the Address Pointer at one of four locations, as indicated by ‘n,m’. Note: The associated RX flag bit (CANINTF.RXnIF) will be cleared after bringing CS high.
-#define RX_BUFFER0 0b00000000
-#define RX_BUFFER1 0b00000001
-#define RX_BUFFER_FROMTOP 0b00000001
-#define RX_BUFFER_FROMBOTTOM 0b00000000
-
-
-#define WRITE 0b00000010 //Write data to register beginning at selected address.
-#define LOAD_TX_BUFFER 0b01000000 //0abc //When loading a transmit buffer, reduces the overhead of a normal Write command by placing the Address Pointer at one of six locations as indicated by ‘a,b,c’.
-#define TX_BUFFER0 0b00000000
-#define TX_BUFFER1 0b00000001
-#define TX_BUFFER2 0b00000010
-#define TX_BUFFER_FROMTOP 0b00000001
-#define TX_BUFFER_FROMBOTTOM 0b00000000
-
-#define RTS 0b10000000 //0nnn //Instructs controller to begin message transmission sequence for any of the transmit buffers.
-#define RTS_BUFFER0 0b00000001
-#define RTS_BUFFER1 0b00000010
-#define RTS_BUFFER2 0b00000100
-#define READ_STATUS 0b10100000 //Quick polling command that reads several status bits for transmit and receive functions.
-#define RX_STATUS 0b10110000 //Quick polling command that indicates filter match and message type (standard, extended and/or remote) of received message.
-#define BIT_MODIFY 0b00000101 
-#define Normal_Operation_mode 0b000
-#define Sleep_mode 0b001 //= Set Sleep mode
-#define Loopback_mode 0b01000000 //= Set Loopback mode
-#define Listen_mode 0b011 //= Set Listen-Only mode
-#define Configuration_mode 0b100 //= Set Configuration mode
-#define Mode_address 0x0e //upper data for mode register, next selects bits to be modified
-#define Mode_mask 0b11100000 //Mask for mode register
-#define Mode_data 0b00000000 //Data for mode register
-
-
-void mcp2515_init();
-
-
 void mcp_reset(){
 	PORTB &= ~(1<<PD4);
 	SPI_Transmit(RESET);
 	PORTB |= (1<<PD4);
-	printf("Reset sent\n\r");
+	debugging(("Reset sent\n\r"));
 }
 
 char mcp_read(char address){
@@ -54,7 +17,7 @@ char mcp_read(char address){
 	SPI_Transmit(address);
 	int byte = SPI_Receive();
 	PORTB |= (1<<PD4);
-	printf("Recived: %x from %x \n\r", byte, address);
+	debugging(("Recived: %x from %x \n\r", byte, address));
 	return byte;
 }
 
@@ -64,7 +27,7 @@ void mcp_write(char address, char data){
 	SPI_Transmit(address);
 	SPI_Transmit(data);
 	PORTB |= (1<<PD4);
-	printf("Wrote %x to %x\n\r" ,data, address);
+	debugging(("Wrote %x to %x\n\r" ,data, address));
 }
 
 void mcp_bit_modify(char address, char mask, char data){
@@ -74,7 +37,7 @@ void mcp_bit_modify(char address, char mask, char data){
 	SPI_Transmit(mask);
 	SPI_Transmit(data);
 	PORTB |= (1<<PD4);
-	printf("Bit modify sent\n\r");
+	debugging(("Bit modify sent\n\r"));
 }
 
 char mcp_read_status(){
@@ -82,9 +45,16 @@ char mcp_read_status(){
 	SPI_Transmit(READ_STATUS);
 	int byte = SPI_Receive();
 	int byte2 = SPI_Receive();
-	PORTB |= (1<<PD4);
-	printf("Status: %x repeat %x\n\r", byte, byte2);
+    PORTB |= (1<<PD4);
+    if (byte == byte2)
+    {
+	debugging(("Status: %x equal to %x\n\r", byte, byte2));
 	return byte;
+    }
+    else {
+        debugging(("Status: %x not equal %x\n\r", byte, byte2));
+        return 0;
+    }
 }
 
 char mcp_read_rx_buffer(char buffer, char startpoint){
@@ -94,7 +64,7 @@ char mcp_read_rx_buffer(char buffer, char startpoint){
 	SPI_Transmit(address);
 	int byte = SPI_Receive();
 	PORTB |= (1<<PD4);
-	printf("RXBUFFER: %x from %x \n\r", byte, address);
+	debugging(("RXBUFFER: %x from %x \n\r", byte, address));
 	return byte;
 }
 
@@ -105,7 +75,7 @@ void mcp_load_tx_buffer(char buffer, char startpoint, char data){
 	SPI_Transmit(address);
 	SPI_Transmit(data);
 	PORTB |= (1<<PD4);
-	printf("RXBUFFER: %x from %x \n\r", data, address);
+	debugging(("RXBUFFER: %x from %x \n\r", data, address));
 }
 
 void mcp_rts(char buffer){
@@ -121,6 +91,8 @@ void mcp_rts(char buffer){
 	int byte = SPI_Receive();
 	int byte2 = SPI_Receive();
 	PORTB |= (1<<PD4);
+    if (byte == byte2)
+    {
 	switch (byte & 0b11000000){
 		case 00:
 		printf("No RX message\n\r");
@@ -177,7 +149,12 @@ void mcp_rts(char buffer){
 		printf("RXF1 rollover to RXB1\n\r");
 		break;
 	}
-	return byte;
+    return byte;
+    }
+    else {
+        debugging(("Status: %x not equal %x\n\r", byte, byte2));
+        return 0;
+    }
  }
 
 void mcp2515_init() {
