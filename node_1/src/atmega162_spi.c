27de420a2dbb156d014c88a077a414fd593a4319
Kristiss99 <keltius87@gmail.com>
2022-10-14 13:52:11 +0200

    mcp done in main

--- node_1/src/atmega162_spi.c
+++ node_1/src/atmega162_spi.c
@@ -3,7 +3,7 @@
 #define ATMEGA162_SPI_IMPORT
 #include "../include/atmega162_spi.h"
 
-char spi_tranceive(char data='\0') {
+char spi_tranceive(char data) {
     SPDR = data;
     while(!(SPSR & (1<<SPIF)));
     return SPDR;
--- node_1/src/main.c
+++ node_1/src/main.c
@@ -6,12 +6,28 @@
 #include <stdio.h>	// Needed for printf
 
 
+
 #define RESET 0b11000000 //Resets internal registers to default state, set Configuration mode.
 #define READ 0b00000011 //Read data from register beginning at selected address.
-#define READ_RX_BUFFER 0b1001 //0nm0 //When reading a receive buffer, reduces the overhead of a normal Read command by placing the Address Pointer at one of four locations, as indicated by ‘n,m’. Note: The associated RX flag bit (CANINTF.RXnIF) will be cleared after bringing CS high.
+#define READ_RX_BUFFER 0b10010000//0nm0 //When reading a receive buffer, reduces the overhead of a normal Read command by placing the Address Pointer at one of four locations, as indicated by ‘n,m’. Note: The associated RX flag bit (CANINTF.RXnIF) will be cleared after bringing CS high.
+#define RX_BUFFER0 0b00000000
+#define RX_BUFFER1 0b00000001
+#define RX_BUFFER_FROMTOP 0b00000001
+#define RX_BUFFER_FROMBOTTOM 0b00000000
+
+
 #define WRITE 0b00000010 //Write data to register beginning at selected address.
-#define LOAD_TX_BUFFER 0b0100 //0abc //When loading a transmit buffer, reduces the overhead of a normal Write command by placing the Address Pointer at one of six locations as indicated by ‘a,b,c’.
-#define RTS 0b1000 //0nnn //Instructs controller to begin message transmission sequence for any of the transmit buffers.
+#define LOAD_TX_BUFFER 0b01000000 //0abc //When loading a transmit buffer, reduces the overhead of a normal Write command by placing the Address Pointer at one of six locations as indicated by ‘a,b,c’.
+#define TX_BUFFER0 0b00000000
+#define TX_BUFFER1 0b00000001
+#define TX_BUFFER2 0b00000010
+#define TX_BUFFER_FROMTOP 0b00000001
+#define TX_BUFFER_FROMBOTTOM 0b00000000
+
+#define RTS 0b10000000 //0nnn //Instructs controller to begin message transmission sequence for any of the transmit buffers.
+#define RTS_BUFFER0 0b00000001
+#define RTS_BUFFER1 0b00000010
+#define RTS_BUFFER2 0b00000100
 #define READ_STATUS 0b10100000 //Quick polling command that reads several status bits for transmit and receive functions.
 #define RX_STATUS 0b10110000 //Quick polling command that indicates filter match and message type (standard, extended and/or remote) of received message.
 #define BIT_MODIFY 0b00000101 
@@ -20,8 +36,9 @@
 #define Loopback_mode 0b01000000 //= Set Loopback mode
 #define Listen_mode 0b011 //= Set Listen-Only mode
 #define Configuration_mode 0b100 //= Set Configuration mode
-#define Mode_address 0x0f //upper data for mode register, next selects bits to be modified
+#define Mode_address 0x0e //upper data for mode register, next selects bits to be modified
 #define Mode_mask 0b11100000 //Mask for mode register
+#define Mode_data 0b00000000 //Data for mode register
 
 
 void mcp2515_init();
@@ -32,7 +49,7 @@ void SPI_init(){
 	DDRB |= (1<<DDB5)|(1<<DDB7)|(1<<DDB4);
 	PORTB |= (1<<PB4);
 	/* Enable SPI, Master, set clock rate fck/16 */
-	SPCR |= (1<<SPE)|(1<<MSTR)|(1<<SPR0);
+	SPCR |= (1<<SPE)|(1<<MSTR)|(1<<SPR1)|(1<<SPR0);
 }
 
 
@@ -52,11 +69,152 @@ char SPI_Receive(void){
 	return SPDR;
 }
 
+void mcp_reset(){
+	PORTB &= ~(1<<PD4);
+	SPI_Transmit(RESET);
+	PORTB |= (1<<PD4);
+	printf("Reset sent\n\r");
+}
+
+char mcp_read(char address){
+	PORTB &= ~(1<<PD4);
+	SPI_Transmit(READ);
+	SPI_Transmit(address);
+	int byte = SPI_Receive();
+	PORTB |= (1<<PD4);
+	printf("Recived: %x from %x \n\r", byte, address);
+	return byte;
+}
+
+void mcp_write(char address, char data){
+	PORTB &= ~(1<<PD4);
+	SPI_Transmit(WRITE);
+	SPI_Transmit(address);
+	SPI_Transmit(data);
+	PORTB |= (1<<PD4);
+	printf("Wrote %x to %x\n\r" ,data, address);
+}
+
+void mcp_bit_modify(char address, char mask, char data){
+	PORTB &= ~(1<<PD4);
+	SPI_Transmit(BIT_MODIFY);
+	SPI_Transmit(address);
+	SPI_Transmit(mask);
+	SPI_Transmit(data);
+	PORTB |= (1<<PD4);
+	printf("Bit modify sent\n\r");
+}
+
+char mcp_read_status(){
+	PORTB &= ~(1<<PD4);
+	SPI_Transmit(READ_STATUS);
+	int byte = SPI_Receive();
+	int byte2 = SPI_Receive();
+	PORTB |= (1<<PD4);
+	printf("Status: %x repeat %x\n\r", byte, byte2);
+	return byte;
+}
+
+char mcp_read_rx_buffer(uint8_t buffer, uint8_t startpoint){
+	uint8_t address = READ_RX_BUFFER;
+	address |= (buffer << 2) | (startpoint << 1);
+	PORTB &= ~(1<<PD4);
+	SPI_Transmit(address);
+	int byte = SPI_Receive();
+	PORTB |= (1<<PD4);
+	printf("RXBUFFER: %x from %x \n\r", byte, address);
+	return byte;
+}
+
+void mcp_load_tx_buffer(uint8_t buffer, uint8_t startpoint, char data){
+	uint8_t address = LOAD_TX_BUFFER;
+	address |= (buffer << 2) | (startpoint << 1);
+	PORTB &= ~(1<<PD4);
+	SPI_Transmit(address);
+	SPI_Transmit(data);
+	PORTB |= (1<<PD4);
+	printf("RXBUFFER: %x from %x \n\r", data, address);
+}
+
+void mcp_rts(uint8_t buffer){
+	uint8_t address = RTS;
+	address |= (buffer << 1);
+	PORTB &= ~(1<<PD4);
+	SPI_Transmit(address);
+	PORTB |= (1<<PD4);
+}
+ char mcp_read_rx_status(){
+	PORTB &= ~(1<<PD4);
+	SPI_Transmit(RX_STATUS);
+	int byte = SPI_Receive();
+	int byte2 = SPI_Receive();
+	PORTB |= (1<<PD4);
+	switch (byte & 0b11000000){
+		case 00:
+		printf("No RX message\n\r");
+		break;
+		case 01:
+		printf("Message in buffer 0\n\r");
+		break;
+		case 10:
+		printf("Message in buffer 1\n\r");
+		break;
+		case 11:
+		printf("Message in both buffers\n\r");
+		break;
+	}
+
+	switch (byte & 0b00011000){
+		case 00:
+		printf("Message is standard\n\r");
+		break;
+		case 01:
+		printf("Message is standard remote\n\r");
+		break;
+		case 10:
+		printf("Message is extended\n\r");
+		break;
+		case 11:
+		printf("Message is extended remote\n\r");
+		break;
+	}
+
+	switch (byte & 0b00000111){
+		case 000:
+		printf("RXF0\n\r");
+		break;
+		case 001:
+		printf("RXF1\n\r");
+		break;
+		case 010:
+		printf("RXF2\n\r");
+		break;
+		case 011:
+		printf("RXF3\n\r");
+		break;
+		case 100:
+		printf("RXF4\n\r");
+		break;
+		case 101:
+		printf("RXF5\n\r");
+		break;
+		case 110:
+		printf("RXF0 rollover to RXB0\n\r");
+		break;
+		case 111:
+		printf("RXF1 rollover to RXB1\n\r");
+		break;
+	}
+	return byte;
+ }
+
+
+
 
 
 int main() {	
-	fdevopen(atmega162_usart_transmit, atmega162_usart_receive); // Enable printf to JTAG
-	atmega162_usart_init();
+	fdevopen(usart_transmit, usart_receive); // Enable printf to JTAG
+	usart_init();
 	// atmega162_sram_init();
 	// atmega162_joystick_init();
 	printf("\n\r\n\rhello\n\r\n\r");
@@ -74,64 +232,54 @@ int main() {
 }
 
 void mcp2515_init() {
-	char address = 0x10;
-	char data = 0xee;
-	char byte;
-	PORTB &= ~(1<<PD4);
-	SPI_Transmit(RESET);
-	PORTB |= (1<<PD4);
-	printf("Reset sent\n\r");
-
-
-	printf("Set Mode \n\r");
-	PORTB &= ~(1<<PD4);
-	SPI_Transmit(BIT_MODIFY);
-	SPI_Transmit(Mode_address);
-	SPI_Transmit(Mode_mask);
-	SPI_Transmit(Loopback_mode);
-	PORTB |= (1<<PD4);
-	printf("Loopback Set \n\r");
 	
+	mcp_reset();
+	mcp_read_status();
+    mcp_bit_modify(Mode_address, Mode_mask, Loopback_mode);
+	mcp_read_status();
 	
-	PORTB &= ~(1<<PD4);
-	SPI_Transmit(READ);
-	SPI_Transmit(Mode_address);
-	byte = SPI_Receive();
-	PORTB |= (1<<PD4);
-	printf("CANCTRL: %x \n\r", byte);
+	mcp_read(Mode_address);
 	
-	while(1){
-	PORTB &= ~(1<<PD4);
-	SPI_Transmit(READ);
-	SPI_Transmit(0x0e);
-	byte = SPI_Receive();
-	PORTB |= (1<<PD4);
-	printf("CANSTAT: %x \n\r", byte);
-	}
+	mcp_read(0x0f);
+	mcp_read(0x0e);
+	mcp_read(0x7f);
+	mcp_read(0x7e);
+
+	mcp_write(0x10, 0xaa);
+	mcp_write(0x20, 0xbb);
+	mcp_write(0x30, 0xcc);
+	mcp_write(0x40, 0xee);
+	mcp_write(0x50, 0xff);
+
+	mcp_read(0x10);
+	mcp_read(0x20);
+	mcp_read(0x30);
+	mcp_read(0x40);
+	mcp_read(0x50);
+
+	mcp_read(0x0f);
+	mcp_read_status(0x0f);
+	mcp_read(0x0e);
+	mcp_read_status(0x0e);
+
+	mcp_load_tx_buffer(TX_BUFFER0, TX_BUFFER_FROMBOTTOM, 0x01);
+	mcp_load_tx_buffer(TX_BUFFER0, TX_BUFFER_FROMTOP, 0x02);
+	mcp_load_tx_buffer(TX_BUFFER1, TX_BUFFER_FROMBOTTOM, 0x03);
+	mcp_load_tx_buffer(TX_BUFFER1, TX_BUFFER_FROMTOP, 0x04);
+	mcp_load_tx_buffer(TX_BUFFER2, TX_BUFFER_FROMBOTTOM, 0x05);
+	mcp_load_tx_buffer(TX_BUFFER2, TX_BUFFER_FROMTOP, 0x06);
 	
-	
 
-	PORTB = (0<<PD4);
-	SPI_Transmit(WRITE);
-	SPI_Transmit(address);
-	SPI_Transmit(data);
-	PORTB = (1<<PD4);
+	mcp_read_rx_buffer(RX_BUFFER0, RX_BUFFER_FROMBOTTOM);
+	mcp_read_rx_buffer(RX_BUFFER0, RX_BUFFER_FROMTOP);
+	mcp_read_rx_buffer(RX_BUFFER1, RX_BUFFER_FROMBOTTOM);
+	mcp_read_rx_buffer(RX_BUFFER1, RX_BUFFER_FROMTOP);
 
+	mcp_rts(RTS_BUFFER0);
 
-	printf("Wrote: %x\n\r", data);
-	PORTB = (0<<PD4);
-	SPI_Transmit(READ_STATUS);
-	byte = SPI_Receive();
-	printf("Status: %x \n\r", byte);
-	PORTB = (1<<PD4);
+	mcp_read_rx_status();
 
 
-	PORTB = (0<<PD4);
-	SPI_Transmit(READ);
-	SPI_Transmit(address);
-	byte = SPI_Receive();
-	PORTB = (1<<PD4);
-	printf("Recived: %x \n\r", byte);
 	
 
 	
