ee52a19e14443f67679ee0c14144629b87877d46
Kristiss99 <keltius87@gmail.com>
2022-10-14 14:17:33 +0200

    Made main -> libraries

--- node_1/include/atmega162_mcp2515.h
+++ node_1/include/atmega162_mcp2515.h
@@ -5,13 +5,42 @@
 #ifndef ATMEGA162_MCP2515_H
 #define ATMEGA162_MCP2515_H
 
+
+#include "../include/atmega162_spi.h"
 #include <avr/io.h>
+#include <stdio.h>	// Needed for printf
 
 #ifdef ATMEGA162_MCP2515_IMPORT
     #define EXTERN
 #else
     #define EXTERN extern
 #endif
+
+void mcp_init();
+
+void mcp_reset();
+
+void mcp_write(char address, char data);
+
+char mcp_read(char address);
+
+void mcp_rts(char buffer);
+
+void mcp_bit_modify(char address, char mask, char data);
+
+char mcp_read_status();
+
+void mcp_rx_status();
+
+char mcp_read_rx_buffer(char buffer, char startpoint);
+
+void mcp_load_tx_buffer(char buffer, char startpoint, char data);
+
+void mcp_read_tx_buffer(char buffer, char startpoint);
+
+
+
+
 
 
 #undef ATMEGA162_MCP2515_IMPORT
--- node_1/src/atmega162_spi.c
+++ node_1/src/atmega162_spi.c
@@ -8,3 +8,28 @@ char spi_tranceive(char data) {
     while(!(SPSR & (1<<SPIF)));
     return SPDR;
 }
+
+void SPI_init(){
+	/* Set MOSI and SCK output and SS, all others input */
+	DDRB |= (1<<DDB5)|(1<<DDB7)|(1<<DDB4);
+	PORTB |= (1<<PB4);
+	/* Enable SPI, Master, set clock rate fck/16 */
+	SPCR |= (1<<SPE)|(1<<MSTR)|(1<<SPR1)|(1<<SPR0);
+}
+
+
+void SPI_Transmit(char data){
+	/* Start transmission */
+	SPDR = data;
+	/* Wait for transmission complete */
+	while(!(SPSR & (1<<SPIF)));
+	
+}
+
+
+char SPI_Receive(void){
+	SPDR = 0xFF;
+	while(!(SPSR & (1<<SPIF)));
+	/* Return data register */
+	return SPDR;
+}
