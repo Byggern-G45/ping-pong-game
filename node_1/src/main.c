d5328e5fd8371fe0239becf3d97d449d98e8446e
espi3000 <espen.holsen.eh@gmail.com>
2022-10-21 14:49:29 +0200

    Started CAN

--- node_1/src/main.c
+++ node_1/src/main.c
@@ -4,6 +4,7 @@
 #include "../include/atmega162_oled.h"
 #include "../include/atmega162_mcp2515.h"
 #include "../include/atmega162_spi.h"
+#include "../include/atmega162_can.h"
 #include <avr/io.h>
 #include <stdio.h>	// Needed for printf
 
@@ -12,18 +13,45 @@
 int main() {	
 	fdevopen(usart_transmit, usart_receive); // Enable printf to JTAG
 	usart_init();
-	// atmega162_sram_init();
-	// atmega162_joystick_init();
-	printf("\n\r\n\rhello\n\r\n\r");
-	printf("SPI_init\n\r");
-	SPI_init();
-	printf("SPI_INITED\n\r");
-	printf("MPC2515_INITED\n\r");
-	mcp2515_init();
-	printf("MPC2515_DONE\n\r");
+	can_init();
+	
+	can_message_t tx_msg;
+	can_message_t rx_msg;
+
+	tx_msg.id = 0x7ff;
+	tx_msg.length = 6;
+	tx_msg.data[0] = 'H';
+	tx_msg.data[1] = 'e';
+	tx_msg.data[2] = 'l';
+	tx_msg.data[3] = 'l';
+	tx_msg.data[4] = 'o';
+	tx_msg.data[5] = '\0';
+	
+	can_message_send(&tx_msg);
+
+	printf("\n\n");
+	printf("CTRL:			%x\n", mcp_read(TXB0REG - 1));
+	printf("STORED ID:      %x%x\n", mcp_read(TXB0REG) << 3, mcp_read(TXB0REG + 1) >> 5);
+    printf("STORED LENGTH:  %d\n", mcp_read(TXB0REG + 4));
+    printf("STORED DATA:    ");
+    for (uint8_t i = 0; i < tx_msg.length; i++) {
+        printf("%c", mcp_read(TXB0REG + 5 + i));
+    }  
+    printf("\n\n");
+
+	
+	rx_msg = can_message_receive();
+
+	printf("RECEIVED ID:    %x\n", rx_msg.id);
+	printf("RECEIVED LENGTH:%d\n", rx_msg.length);
+	printf("RECEIVED DATA:  ");
+	for (uint8_t i = 0; i < rx_msg.length; i++) {
+		printf("%c", rx_msg.data[i]);
+	}
+	printf("\n\n");
 	
 	
-	// while (1) {
+	while (1); 
 	// 	atmega162_joystick_start_conversion();
     // }
 }
