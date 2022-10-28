8961d8c502abec9bd015b34cf83d24c073c3d9f7
espi3000 <espen.holsen.eh@gmail.com>
2022-10-28 11:01:52 +0200

    Made project

--- node_2/src/main.c
+++ node_2/src/main.c
@@ -1,28 +1,32 @@
-#include "sam.h"
-#include "uart.h"
-#include "timer.h"
+#include <sam.h>
+//#include "uart.h"
+//#include "timer.h"
+//#include <utim>
 
 
 int main(void)
 {
-	configure_uart();
+	//configure_uart();
 
 	/* Initialize the SAM system */
 	SystemInit();
-	PIOA->PIO_OER = 0x1u <<20;
+	PIOA->PIO_OER = 1<<20;
 
-	PIOA->PIO_OER = 0x1u <<19;
+	PIOA->PIO_OER = 1<<19;
 
-	PIOA->PIO_PER = 0x1u<<0;
-	PIOA->PIO_PER = 0x1u<<1;
-	PIOA->PIO_SODR = 0x1u<<19;
+	PIOA->PIO_PER = 1<<0;
+	PIOA->PIO_PER = 1<<1;
+	
+	//PIOA->PIO_SODR = 1<<19;
+	
+	PIOA->PIO_CODR = 0xffffffff;
 
 	while (1)
 	{
-		PIOA->PIO_SODR = 0x1u<<20;
-		delay_ms(1000);
-		PIOA->PIO_CODR = 0x1u<<20;
-		delay_ms(1000);
+		PIOA->PIO_SODR = 1<<20;
+		//delay_ms(1000);
+		//PIOA->PIO_CODR = 0x1u<<20;
+		//delay_ms(1000);
 	}
 
 }
