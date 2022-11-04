bbb845a00bee5602805f11e0f755afde649c91b0
Birger Maelen <birger.99@live.no>
2022-11-04 17:10:00 +0100

    wdw

--- node_2/src/main.c
+++ node_2/src/main.c
@@ -1,32 +1,40 @@
 #include <sam.h>
+#include <sam3x8e.h>
+#include "../include/pwm.h"
 //#include "uart.h"
-//#include "timer.h"
 //#include <utim>
 
-
-int main(void)
-{
+int main(void) {
 	//configure_uart();
 
 	/* Initialize the SAM system */
 	SystemInit();
-	PIOA->PIO_OER = 1<<20;
-
-	PIOA->PIO_OER = 1<<19;
-
-	PIOA->PIO_PER = 1<<0;
-	PIOA->PIO_PER = 1<<1;
-	
-	//PIOA->PIO_SODR = 1<<19;
-	
-	PIOA->PIO_CODR = 0xffffffff;
 
 	while (1)
 	{
-		PIOA->PIO_SODR = 1<<20;
-		//delay_ms(1000);
-		//PIOA->PIO_CODR = 0x1u<<20;
-		//delay_ms(1000);
 	}
+}
 
+void IR_init(){
+	
+}
+
+bool goal_reg(){
+	
+}
+
+void excercise7_blinkLED_init(){
+	PIOA->PIO_OER |= 1<<20;
+	PIOA->PIO_OER |= 1<<19;
+	PIOA->PIO_PER |= 1<<20;
+	PIOA->PIO_PER |= 1<<19;
+}
+
+void excercise7_blink_led(){
+	PIOA->PIO_SODR |= 1<<20;
+	PIOA->PIO_SODR |= 1<<19;
+	delay(1000);
+	PIOA->PIO_CODR |= 1<<20;
+	PIOA->PIO_CODR |= 1<<19;
+	delay(1000);
 }
