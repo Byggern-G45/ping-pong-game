9c7a1720713571f4a10cebf1519d1f68f71aad22
Kristiss99 <keltius87@gmail.com>
2022-11-12 16:03:56 +0100

    Made timer work

--- node_2/src/timer.c
+++ node_2/src/timer.c
@@ -4,23 +4,21 @@
 #include "../include/timer.h"
 
 void timer_init(void){
-	PMC->PMC_PCER0 |= 1<<27; //Enable clock for TC0
+	PMC->PMC_PCER0 |= PMC_PCER0_PID27; //Enable clock for TC0
 
 
-	// PIOB->PIO_PDR |=PIO_PDR_P0;//set PB25(PWM2) as output
-	// PIOB->PIO_ABSR |=PIO_ABSR_P0;//Peripheral select B
-
-	TC0->TC_CHANNEL[1].TC_CMR |= TC_CMR_TCCLKS_TIMER_CLOCK1|//Timer clock 1 = MCK/2
-                                 TC_CMR_WAVE| //Waveform mode
+	TC0->TC_CHANNEL[0].TC_CMR |= TC_CMR_WAVE| //Waveform mode
                                  TC_CMR_WAVSEL_UP_RC| //UP mode with automatic trigger on RC Compare
                                  TC_CMR_ACPA_CLEAR| //Set TIOA to 0 when RC Compare
                                  TC_CMR_ACPC_SET; //Set TIOA to 1 when RC Compare
 
-	TC0->TC_CHANNEL[1].TC_RC = 840000; //Set RC Compare to 840000, ca 60Hz
+	TC0->TC_CHANNEL[0].TC_RC = 840000; //Set RC Compare to 840000, ca 60Hz
 
-	TC0->TC_CHANNEL[1].TC_CCR |= TC_CCR_SWTRG| //Start timer software trigger
-                                 TC_CCR_CLKEN; //Enable clock
+	TC0->TC_CHANNEL[0].TC_CCR |= TC_CCR_CLKEN|//Start timer software trigger
+								 TC_CCR_SWTRG; //Enable clock
+                               
 
-	TC0->TC_CHANNEL[1].TC_IER |= TC_IER_CPCS; //Enable interrupt on RC Compare
+	TC0->TC_CHANNEL[0].TC_IER |= TC_IER_CPCS; //Enable interrupt on RC Compare
 	NVIC_EnableIRQ(TC0_IRQn); //Enable interrupt in NVIC
+
 }
