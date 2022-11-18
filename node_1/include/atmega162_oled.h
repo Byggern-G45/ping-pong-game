8c421a1b7a42ef034f8108247d720ee6f4db640b
espi3000 <espen.holsen.eh@gmail.com>
2022-11-18 20:09:38 +0100

    Commented oled

--- node_1/include/atmega162_oled.h
+++ node_1/include/atmega162_oled.h
@@ -13,24 +13,62 @@
     #define EXTERN extern
 #endif
 
+/**
+ * @brief Write command to OLED
+ * @param command 
+ */
 void write_c(uint8_t command);
 
+/**
+ * @brief Write data to OLED
+ * @param command 
+ */
 void write_d(uint8_t command);
 
+/**
+ * @brief Initialize OLED
+ */
 void oled_init();
 
+/**
+ * @brief Clear OLED
+ */
 void oled_reset();
 
+/**
+ * @brief Sets cursor to (0,0)
+ */
 void oled_home();
 
+/**
+ * @brief Sets cursor to first in row
+ * @param line row
+ */
 void oled_goto_line(uint8_t line);
 
+/**
+ * @brief Sets cursor to column
+ * @param column
+ */
 void oled_goto_column(uint8_t column);
 
+/**
+ * @brief Clears a row
+ * @param line row
+ */
 void oled_clear_line(uint8_t line);
 
+/**
+ * @brief Sets cursor to (line,column)
+ * @param line row
+ * @param column
+ */
 void oled_pos(uint8_t line, uint8_t column);
 
+/**
+ * @brief Write a string to OLED
+ * @param data, string
+ */
 void oled_print(char* data);
 
 #undef ATMEGA162_OLED_IMPORT
